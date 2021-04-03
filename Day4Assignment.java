92c4f38192cdf16ee808698c5ed1e7b5540f0e75
vinothprabha <81847962+vinothprabha@users.noreply.github.com>
2021-04-03 12:23:00 +0530

    Day4Assigement

---
+++ Day4Assignment.java
@@ -0,0 +1,26 @@
+package org.day4.gettext;
+
+import java.util.Scanner;
+
+public class Day4Assignment {
+public static void main(String[] args) {
+	for(int num=1; num<=100; num++) {
+		
+		int i,j=0;
+		
+		int a=num;
+		
+		while(a>0) {
+			i=a%10;
+			j=(j*10)+i;
+			a=a/10;
+		}
+		
+				if(num==j) {
+			System.out.println(j);
+		}
+	}
+	
+	
+}
+}
