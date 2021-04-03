965642a1ccc9b03fa1d6e1d195f4210360985941
vinothprabha <81847962+vinothprabha@users.noreply.github.com>
2021-04-03 12:13:59 +0530

    Initial commit

---
+++ Employee.java
@@ -0,0 +1,30 @@
+package org.polymorphism;
+
+import java.util.Scanner;
+
+public class Employee {
+private void empId(String name) {
+	 System.out.println("the employee name is " +name);
+}
+private void empId(int id) {
+	System.out.println("the employee id is " +id);
+}
+private void empId(long phno) {
+System.out.println("the employee phone number is " +phno);
+}
+public static void main(String[] args) {
+	
+	Scanner s=new Scanner(System.in);
+	Employee d=new Employee();
+String name=s.next();
+	d.empId(name);
+int id=s.nextInt();
+	d.empId(id);
+long phno=s.nextLong();
+	d.empId(phno);
+
+	
+}
+
+
+}
